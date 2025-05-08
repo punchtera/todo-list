46055d683074d6bc63b94d7b0c32147e9e341a7f
Juan <juanramirezcastaneda@gmail.com>
2025-05-08 16:42:27 -0500

    Default unit test using typescript working

---
+++ src/components/TaskList.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom"; // For custom matchers like toBeInTheDocument
+
+describe("Greeting Component", () => {
+  it("renders the greeting message with the provided name", () => {
+    expect(true).toEqual(true);
+  });
+});
